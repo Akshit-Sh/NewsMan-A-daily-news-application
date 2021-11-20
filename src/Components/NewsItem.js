bb9892520c658b8842da705a4abc8f4beacdec1f
akshit sharma <akshitsharma20022@gmail.com>
2021-11-20 21:55:47 +0530

    modified the design

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  
-    render() {
-       let {title,description, imageUrl, newsUrl} = this.props;
-        return (
-            <div className="my-3">
-                <div className="card">
-  <img src={!imageUrl?"https://images.hindustantimes.com/img/2021/11/11/1600x900/XFZUPMGTYZKA3HFBEL3FEU3LXU_1636600446224_1636600484831.jpg":imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read more</a>
-  </div>
-</div>
-            </div>
-        )
-    }
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-4">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://images.hindustantimes.com/img/2021/11/11/1600x900/XFZUPMGTYZKA3HFBEL3FEU3LXU_1636600446224_1636600484831.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;
