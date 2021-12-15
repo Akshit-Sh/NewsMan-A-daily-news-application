fb41f4a9872b8b83f7aeaf428efaf3bca6d292de
akshit sharma <akshitsharma20022@gmail.com>
2021-12-15 14:12:37 +0530

    added a top loading bar

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
+import LoadingBar from "react-top-loading-bar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
   pageSize = 15;
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <Router>
           <Navbar />
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+            // onLoaderFinished={() => setProgress(0)}
+          />
           <Switch>
             <Route exact path="/">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="general"
                 pageSize={this.pageSize}
                 country="in"
@@ -27,6 +40,7 @@ export default class App extends Component {
             <Route exact path="/Business">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="business"
                 pageSize={this.pageSize}
                 country="in"
@@ -38,6 +52,7 @@ export default class App extends Component {
             <Route exact path="/entertainment">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="entertainment"
                 pageSize={this.pageSize}
                 country="in"
@@ -49,6 +64,7 @@ export default class App extends Component {
             <Route exact path="/health">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="health"
                 pageSize={this.pageSize}
                 country="in"
@@ -60,6 +76,7 @@ export default class App extends Component {
             <Route exact path="/science">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="science"
                 pageSize={this.pageSize}
                 country="in"
@@ -71,6 +88,7 @@ export default class App extends Component {
             <Route exact path="/sports">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="sports"
                 pageSize={this.pageSize}
                 country="in"
@@ -82,6 +100,7 @@ export default class App extends Component {
             <Route exact path="/technology">
               {" "}
               <News
+                setProgress={this.setProgress}
                 key="technology"
                 pageSize={this.pageSize}
                 country="in"
--- src/Components/News.js
+++ src/Components/News.js
@@ -34,6 +34,7 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6475ed9e488f4260b7bc6826a0bdc106&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -44,6 +45,7 @@ export class News extends Component {
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
